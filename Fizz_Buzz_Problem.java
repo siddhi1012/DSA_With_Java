83d2a12a4300fb30766ba52216dae1421cd0efd7
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-02-20 10:55:31 +0530

    via upload

--- Fizz_Buzz_Problem.java
+++ Fizz_Buzz_Problem.java
@@ -1,31 +1,31 @@
-import java.util.*;
-
 public class Fizz_Buzz_Problem {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
+
         int n = 15;
         int counter3 = 0;
         int counter5 = 0;
 
         for (int i = 1; i <= n; i++) {
+
             counter3++;
             counter5++;
+
             if (counter3 == 3 && counter5 == 5) {
-                System.out.println("FizzBuzz");
+                System.out.print("FizzBuzz ");
                 counter3 = 0;
                 counter5 = 0;
-            } else if (counter3 == 3) {
-                System.out.println("Fizz");
-                counter3 = 0;
-
-            } else if (counter5 == 5) {
-                System.out.println("Buzz");
-                counter5 = 0;
-            } else {
-                System.out.println(i);
             }
-
+            else if (counter3 == 3) {
+                System.out.print("Fizz ");
+                counter3 = 0;
+            }
+            else if (counter5 == 5) {
+                System.out.print("Buzz ");
+                counter5 = 0;
+            }
+            else {
+                System.out.print(i + " ");
+            }
         }
-
     }
 }
